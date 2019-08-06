089a411a7bb5f3c8975f303d8c2c171d9ec186a9
Noven Purnell-Webb <noven.purnellwebb@theappteam.com.au>
2019-08-06 12:41:12 +1000

    delete

--- admin/header-includes.php
+++
@@ -1,6 +0,0 @@
-<link href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700&display=swap" rel="stylesheet">
-
-
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-<link href="https://findsellfulfill.com/app/main.css" type="text/css" rel="stylesheet">
-<link href="https://findsellfulfill.com/app/admin/style.css" type="text/css" rel="stylesheet">
--- user/lists/connectShopify.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-
-$shopname = $_POST["shopname"];
-$shopurl = "https://findsellfulfill.com/app/install.php?shop=".$shopname;
-header("Location: $shopurl");
-die();
-
-?>
